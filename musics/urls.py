a74a819e6cce85a610c7bc44be9e2890ff9fdf33
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-20 21:28:56 +0000

    views and url completed

--- musics/urls.py
+++ musics/urls.py
@@ -1,9 +1,9 @@
-from musics.views import homePage, musicList
+from musics.views import addMusic, homePage, musicList
 from django.urls import path
 
 app_name='musics'
 
 urlpatterns = [
-    path('',homePage,name='home_page'),
-    path('list/',musicList,name='music_list'),    
+    path('',homePage,name='home_page'), 
+    path('add/',addMusic,name='add_music'),  
 ]
--- musics/views.py
+++ musics/views.py
@@ -1,10 +1,36 @@
-from musics.models import Music
-from django.shortcuts import render
+from musics.models import Album, Music
+from django.shortcuts import redirect, render
 from django.http import JsonResponse
+from .form import AddMusicForm
 
 def homePage(request):
-    return render(request,'home.html')
+    musics=list(Music.objects.all().values())
+    return render(request,'home.html',{
+        'musics':musics
+    })
 
-def musicList(request):
-    music=Music.objects.all().values()
-    return JsonResponse(list(music),safe=False)
+def addMusic(request):
+    form=AddMusicForm()
+
+    if request.POST:
+        form=AddMusicForm(request.POST,request.FILES)
+     
+        if form.is_valid():
+            instance=form.save(commit=False)
+            album=form.cleaned_data.get('album')
+            if album:
+                music_album=Album.objects.get_or_create(name=album)
+                print(music_album)
+                instance.album=music_album[0]
+                instance.save()
+                return redirect("music:home_page")
+            else:
+                instance.save()
+                return redirect("music:home_page")
+
+        else:
+            print("no",form.data)
+    
+    return render(request,'addPage.html',{
+        'form':form
+    })
