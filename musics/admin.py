b7823bc85f056c62ebd8480e9868feca1d52b1f9
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-21 19:01:46 +0000

    starter files for proj

--- musics/admin.py
+++ musics/admin.py
@@ -1,5 +1,2 @@
 from django.contrib import admin
-from .models import Music,Album
 
-admin.site.register(Music)
-admin.site.register(Album)
--- musics/urls.py
+++ musics/urls.py
@@ -1,9 +1,8 @@
-from musics.views import addMusic, homePage
+
 from django.urls import path
 
 app_name='musics'
 
 urlpatterns = [
-    path('',homePage,name='home_page'), 
-    path('add/',addMusic,name='add_music'),  
+   
 ]
